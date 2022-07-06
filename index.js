2ffc1ce0ebb34ff968f31783577419ad13edd596
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-07-06 16:59:41 +0000

    Modified index.js

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm 
+  /* 
+  convert the input word an array and use the reverse method to reverse it
+  convert it back to string and assign it to a new variable
+  if new variable = input word
+    then return true
+  else
+      then return false
+
+*/
+const wordArray = word.split("");
+const reverseArray = wordArray.reverse();
+const convertedWord = reverseArray.join("")
+if (word === convertedWord){
+  return true;
+}else{
+  return false;
+}
+ 
+
+
+
+
 }
 
 /* 
   Add your pseudocode here
+*/ 
+/* 
+  make a copy of the input string
+  convert it a an array and the reverse method the reverse it
+  convert it back to string and assing it to a new variable
+  if new variable = input string
+    then return true
+  else
+      then return false
+
 */
 
 /*
@@ -14,6 +46,10 @@ function isPalindrome(word) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
+  console.log("=>", isPalindrome("dad"));
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("sad"));
+  console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
@@ -23,3 +59,5 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+
